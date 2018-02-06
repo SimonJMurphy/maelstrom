5b312e7960a35a81d3190b1bec33c8be536ce6f4
Dan F-M <danfm@nyu.edu>
2018-02-06 16:57:25 -0500

    changes

--- maelstrom/maelstrom.py
+++ maelstrom/maelstrom.py
@@ -25,13 +25,13 @@ class Maelstrom(object):
     T = tf.float64
 
     def __init__(self, time, mag, nu, log_sigma2=None, with_eccen=True):
-        self.time = np.atleast_1d(time)
-        self.mag = np.atleast_1d(mag)
-        self.nu = np.atleast_1d(nu)
+        self.time_data = np.atleast_1d(time)
+        self.mag_data = np.atleast_1d(mag)
+        self.nu_data = np.atleast_1d(nu)
         self.with_eccen = with_eccen
 
-        self.time_tensor = tf.constant(self.time, dtype=self.T)
-        self.mag_tensor = tf.constant(self.mag, dtype=self.T)
+        self.time = tf.constant(self.time_data, dtype=self.T)
+        self.mag = tf.constant(self.mag_data, dtype=self.T)
 
         self._session = None
 
@@ -39,9 +39,11 @@ class Maelstrom(object):
         if log_sigma2 is None:
             log_sigma2 = 0.0
         self.log_sigma2 = tf.Variable(log_sigma2, dtype=self.T)
-        self.nu_tensor = tf.Variable(self.nu, dtype=self.T)
+        self.nu = tf.Variable(self.nu_data, dtype=self.T)
         self.period = tf.Variable(1.0, dtype=self.T)
-        self.lighttime = tf.Variable(np.zeros_like(self.nu), dtype=self.T)
+        self.lighttime = tf.Variable(np.zeros_like(self.nu_data), dtype=self.T)
+        self.lighttime_inds = tf.Variable(
+            np.arange(len(self.nu_data)).astype(np.int32), dtype=tf.int32)
         self.tref = tf.Variable(0.0, dtype=self.T)
         if self.with_eccen:
             self.eccen_param = tf.Variable(-5.0, dtype=self.T)
@@ -56,12 +58,12 @@ class Maelstrom(object):
             self.params += [self.eccen_param, self.varpi]
 
         # Set up the model
-        self.mean_anom = 2.0*np.pi*(self.time_tensor-self.tref)/self.period
+        self.mean_anom = 2.0*np.pi*(self.time-self.tref)/self.period
         if self.with_eccen:
             self.ecc_anom = kepler(self.mean_anom, self.eccen)
             self.true_anom = 2.0*tf.atan2(
                 tf.sqrt(1.0+self.eccen) * tf.tan(0.5*self.ecc_anom),
-                tf.sqrt(1.0-self.eccen) + tf.zeros_like(self.time_tensor))
+                tf.sqrt(1.0-self.eccen) + tf.zeros_like(self.time))
             factor = 1.0 - tf.square(self.eccen)
             factor /= 1.0 + self.eccen*tf.cos(self.true_anom)
             self.psi = -factor * tf.sin(self.true_anom + self.varpi)
@@ -69,25 +71,29 @@ class Maelstrom(object):
             self.psi = -tf.sin(self.mean_anom)
 
         # Build the design matrix
-        arg = 2.0*np.pi*self.nu_tensor[None, :] * (
-            self.time_tensor[:, None] -
-            self.lighttime[None, :] * self.psi[:, None])
+        ad = tf.gather(self.lighttime, self.lighttime_inds)
+        self.tau = ad[None, :] * self.psi[:, None]
+        arg = 2.0*np.pi*self.nu[None, :] * (self.time[:, None] - self.tau)
         D = tf.concat([tf.cos(arg), tf.sin(arg),
-                       tf.ones((len(self.time), 1), dtype=self.T)], axis=1)
+                       tf.ones((len(self.time_data), 1), dtype=self.T)],
+                      axis=1)
 
         # Solve for the amplitudes and phases of the oscillations
         DTD = tf.matmul(D, D, transpose_a=True)
-        DTy = tf.matmul(D, self.mag_tensor[:, None], transpose_a=True)
+        DTy = tf.matmul(D, self.mag[:, None], transpose_a=True)
         W_hat = tf.linalg.solve(DTD, DTy)
 
         # Finally, the model and the chi^2 objective:
         self.model = tf.squeeze(tf.matmul(D, W_hat))
-        self.chi2 = tf.reduce_sum(tf.square(self.mag_tensor - self.model))
+        self.chi2 = tf.reduce_sum(tf.square(self.mag - self.model))
         self.chi2 *= tf.exp(-self.log_sigma2)
-        self.chi2 += len(self.time) * self.log_sigma2
+        self.chi2 += len(self.time_data) * self.log_sigma2
 
         # Initialize all the variables
-        self.get_session().run(tf.global_variables_initializer())
+        self.run(tf.global_variables_initializer())
+
+    def run(self, *args, **kwargs):
+        return self.get_session().run(*args, **kwargs)
 
     def __del__(self):
         if self._session is not None:
@@ -120,7 +126,36 @@ class Maelstrom(object):
             ops.append(tf.assign(self.eccen_param,
                                  np.log(eccen) - np.log(1.0 - eccen)))
             ops.append(tf.assign(self.varpi, varpi))
-        self.get_session().run(ops)
+        self.run(ops)
 
-    def set_parameter_set(self, params):
+    def get_fit_parameters(self):
+        """Get the list of parameters that are being fit"""
+        return self.params
+
+    def set_fit_parameters(self, params):
+        """Set the list of parameters that should be fit
+
+        Args:
+            params: A list of TensorFlow variables that should be fit.
+
+        """
         self.params = params
+
+    def optimize(self, params=None, **kwargs):
+        if params is None:
+            params = self.params
+        kwargs["method"] = kwargs.get("method", "L-BFGS-B")
+        opt = tf.contrib.opt.ScipyOptimizerInterface(self.chi2, params,
+                                                     **kwargs)
+        return opt.minimize(self.get_session())
+
+    def get_lighttime_estimates(self):
+        sigma = 1.0 / tf.sqrt(-tf.diag_part(tf.hessians(-0.5*self.chi2,
+                                                        self.lighttime)[0]))
+        return self.run([self.lighttime, sigma])
+
+    def pin_lighttime_values(self, double=False):
+        if double:
+            pass
+        else:
+            pass
