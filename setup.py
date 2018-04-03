bbc609ce9181d140896b44105497747a4b317eba
Dan F-M <danfm@nyu.edu>
2018-04-03 08:51:41 -0400

    adding rv offset parameter

--- setup.py
+++ setup.py
@@ -5,17 +5,21 @@ import tensorflow as tf
 from setuptools import setup, Extension
 
 
-include_dirs = [".", tf.sysconfig.get_include()]
-include_dirs.append(os.path.join(
-    include_dirs[1], "external/nsync/public"))
+# include_dirs = [".", tf.sysconfig.get_include()]
+# include_dirs.append(os.path.join(
+#     include_dirs[1], "external/nsync/public"))
+
+compile_flags = tf.sysconfig.get_compile_flags()
+compile_flags += ["-std=c++11", "-stdlib=libc++", "-O2", "-undefined dynamic_lookup"]
+link_flags = tf.sysconfig.get_link_flags()
 
 extensions = [
     Extension(
         "maelstrom.kepler.kepler_op",
         sources=["maelstrom/kepler/kepler_op.cc"],
         language="c++",
-        include_dirs=include_dirs,
-        extra_compile_args=["-std=c++11", "-stdlib=libc++"],
+        extra_compile_args=compile_flags,
+        extra_link_args=link_flags,
     ),
     Extension(
         "maelstrom.interp.interp_op",
@@ -23,8 +27,8 @@ extensions = [
             "maelstrom/interp/searchsorted_op.cc",
         ],
         language="c++",
-        include_dirs=include_dirs,
-        extra_compile_args=["-std=c++11", "-stdlib=libc++"],
+        extra_compile_args=compile_flags,
+        extra_link_args=link_flags,
     ),
 ]
 
